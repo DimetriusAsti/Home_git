75371f115bf42c12900dd992161cc7479d0e83b7
Dimas Fistashka <dimolimon566575@gmail.com>
2023-10-22 14:04:32 +0500

    version 2

--- Calc.cpp
+++ Calc.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a;
-    int b;
+    int a, b;
+    cout << "First num(a): ";
     cin >> a;
+    cout << "Second num(b): ";
     cin >> b;
-    cout << a + b;
-    cout << a - b;
+    if (a > b){
+        cout << "a > b" << endl;
+        }
+    else if (a < b){
+        cout << "a < b" << endl;
+        }
+    else{
+        cout << "a = b" << endl;
+        }
+    cout << "Sum: " << a + b;
+
+
+    
     return 0;
 }
